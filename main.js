54686a43d2ca4aaa82e68aece2691354aaac2cfb
emereson <escanorrosa123@gmail.com>
2024-10-12 12:12:10 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,70 @@
+class Firework {
+  constructor(x, y, targetX, targetY, shade, offsprings) {
+    this.dead = false;
+    this.offsprings = offsprings;
+
+    this.x = x;
+    this.y = y;
+    this.targetX = targetX;
+    this.targetY = targetY;
+
+    this.shade = shade;
+    this.history = [];
+  }
+
+  update(delta) {
+    if (this.dead) return;
+
+    let xDiff = this.targetX - this.x;
+    let yDiff = this.targetY - this.y;
+    if (Math.abs(xDiff) > 3 || Math.abs(yDiff) > 3) {
+      // is still moving
+      this.x += xDiff * 2 * delta;
+      this.y += yDiff * 2 * delta;
+
+      this.history.push({
+        x: this.x,
+        y: this.y,
+      });
+
+      if (this.history.length > 20) this.history.shift();
+    } else {
+      if (this.offsprings && !this.madeChilds) {
+        let babies = this.offsprings; // Mantener la cantidad original de offsprings
+        for (let i = 0; i < babies; i++) {
+          // Usar la fórmula para el corazón
+          let angle = i * (PI2 / babies); // Ángulo para la distribución
+          let targetX = this.x + 16 * Math.sin(angle) ** 3; // Ecuación del corazón en X
+          let targetY =
+            this.y -
+            (13 * Math.cos(angle) -
+              5 * Math.cos(2 * angle) -
+              2 * Math.cos(3 * angle) -
+              Math.cos(4 * angle)); // Ecuación del corazón en Y
+
+          birthday.fireworks.push(
+            new Firework(this.x, this.y, targetX, targetY, this.shade, 0)
+          );
+        }
+      }
+      this.madeChilds = true;
+      this.history.shift();
+    }
+
+    if (this.history.length === 0) this.dead = true;
+    else if (this.offsprings) {
+      for (let i = 0; this.history.length > i; i++) {
+        let point = this.history[i];
+        ctx.beginPath();
+        ctx.fillStyle = "hsl(" + this.shade + ",100%," + i + "%)";
+        ctx.arc(point.x, point.y, 1, 0, PI2, false);
+        ctx.fill();
+      }
+    } else {
+      ctx.beginPath();
+      ctx.fillStyle = "hsl(" + this.shade + ",100%,50%)";
+      ctx.arc(this.x, this.y, 1, 0, PI2, false);
+      ctx.fill();
+    }
+  }
+}
